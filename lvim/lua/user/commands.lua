fc9268207a138036c15a1b536ad401b2b3f4a988
Ramiz K <kharoufeh.r@gmail.com>
2024-09-07 14:34:12 +1000

    Add custom command function to replace file content with clipboard

--- lvim/lua/user/commands.lua
+++ lvim/lua/user/commands.lua
@@ -8,3 +8,14 @@ local function save_reload_and_restart_lsp()
   print("Buffer and LSP reloaded")
 end
 vim.api.nvim_create_user_command('SaveReloadAndRestartLsp', save_reload_and_restart_lsp, {})
+
+local function replace_file_with_clipboard()
+  -- Clear the current buffer
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+  -- Paste the clipboard contents
+  vim.cmd('normal! "+p')
+  -- Save the file
+  vim.cmd('w')
+  print("File contents replaced with clipboard")
+end
+vim.api.nvim_create_user_command('ReplaceFileWithClipboard', replace_file_with_clipboard, {})
