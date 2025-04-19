0794b220d48f6187e4e08c97c02b7b46c8aa620a
iamramizk <59407701+iamramizk@users.noreply.github.com>
2025-04-19 20:19:37 +1000

    fix bash/zsh diagnostics, lsp, remove shebang from zshrc/aliases

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+-- zsh as bash for diagnostics
 vim.filetype.add({
   extension = {
     zsh = "sh",
@@ -10,13 +11,49 @@ vim.filetype.add({
 })
 
 return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    opts.diagnostics = {
-      virtual_text = false,
-    }
-    opts.servers = opts.servers or {}
-    opts.servers.cssls = {}
-    opts.servers.bashls = {}
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.diagnostics = {
+        virtual_text = false,
+      }
+      opts.servers = opts.servers or {}
+      opts.servers.cssls = {}
+      -- opts.servers.bashls = {
+      --   filetypes = { "sh", "zsh" }, -- activate for sh and zsh
+      --   settings = { bashIde = { globPattern = "*@(.sh|.inc|.bash|.command|.zsh)" } },
+      -- }
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "css",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      })
+    end,
+  },
+  {
+    "pablos123/shellcheck.nvim",
+    config = function()
+      require("shellcheck-nvim").setup({
+        shellcheck_options = { "-x", "--enable=all" },
+      })
+    end,
+    ft = { "sh", "zsh" },
+  },
 }
