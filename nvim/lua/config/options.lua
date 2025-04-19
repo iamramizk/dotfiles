363a81999b98f4cdf52ed507113624c653d1649c
iamramizk <59407701+iamramizk@users.noreply.github.com>
2025-04-19 10:32:26 +1000

    update diagnostics icons

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,3 +8,22 @@ vim.opt.whichwrap:append("<,>")
 -- Enable line wrapping by default
 vim.opt.wrap = true
 vim.opt.linebreak = true
+
+-- update icons
+local icons = {
+  Error = "",
+  Warn = "",
+  Info = "",
+  Hint = "",
+}
+vim.diagnostic.config({
+  signs = {
+    active = true,
+    text = {
+      [vim.diagnostic.severity.ERROR] = icons.Error,
+      [vim.diagnostic.severity.WARN] = icons.Warn,
+      [vim.diagnostic.severity.INFO] = icons.Info,
+      [vim.diagnostic.severity.HINT] = icons.Hint,
+    },
+  },
+})
