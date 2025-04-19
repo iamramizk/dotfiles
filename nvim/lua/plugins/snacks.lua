7940e308ff702d573523a055548c3cf31338288a
iamramizk <59407701+iamramizk@users.noreply.github.com>
2025-04-20 01:33:25 +1000

    update snacks config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,12 +5,33 @@ return {
       win = {
         input = {
           keys = {
-            ["<Esc>"] = { "close", mode = { "n", "i" } }, -- Close picker on single Esc in normal and insert mode
+            ["<Esc>"] = { "close", mode = { "n", "i" } }, -- Close picker on single Esc
           },
         },
       },
       layout = {
         preset = "ivy",
+      },
+    },
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = "",
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
       },
     },
   },
