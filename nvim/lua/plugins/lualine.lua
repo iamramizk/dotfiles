ddaed9e4740084aa3f309492cd87db785b0ae914
iamramizk <59407701+iamramizk@users.noreply.github.com>
2025-04-22 15:26:29 +1000

    remove lazy.status and update presets for lualine_x

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -78,7 +78,9 @@ return {
     }
 
     -- update icon in branch
-    opts.sections.lualine_b = { { "branch", icon = "" } }
+    opts.sections.lualine_b = {
+      { "branch", icon = "" },
+    }
 
     -- function to get parent paths (2)
     local function two_parent_dirs()
@@ -142,13 +144,65 @@ return {
       },
     }
 
-    table.insert(opts.sections.lualine_x, {
-      "lsp_status",
-      color = { fg = "#79809E" },
-      symbols = {
-        done = "",
+    -- table.insert(opts.sections.lualine_x, {
+    --   "lsp_status",
+    --   color = { fg = "#79809E" },
+    --   symbols = {
+    --     done = "",
+    --   },
+    -- })
+    opts.sections.lualine_x = {
+      Snacks.profiler.status(),
+      -- stylua: ignore
+      {
+        function() return require("noice").api.status.command.get() end,
+        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+        color = function() return { fg = Snacks.util.color("Statement") } end,
       },
-    })
+      -- stylua: ignore
+      {
+        function() return require("noice").api.status.mode.get() end,
+        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+        color = function() return { fg = Snacks.util.color("Constant") } end,
+      },
+      -- stylua: ignore
+      {
+        function() return "  " .. require("dap").status() end,
+        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+        color = function() return { fg = Snacks.util.color("Debug") } end,
+      },
+      -- stylua: ignore
+      -- {
+      --   require("lazy.status").updates,
+      --   cond = require("lazy.status").has_updates,
+      --   color = function() return { fg = Snacks.util.color("Special") } end,
+      -- },
+      {
+        "diff",
+        symbols = {
+          added = icons.git.added,
+          modified = icons.git.modified,
+          removed = icons.git.removed,
+        },
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            }
+          end
+        end,
+      },
+      {
+        "lsp_status",
+        color = { fg = "#79809E" },
+        symbols = {
+          done = "",
+        },
+      },
+    }
 
     opts.sections.lualine_y = {
       { "progress" },
