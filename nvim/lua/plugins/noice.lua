926b04b3cb94b4380adc062ff20fdc4375dabb45
iamramizk <59407701+iamramizk@users.noreply.github.com>
2025-12-01 14:21:04 +1100

    fix noice.nvim; disable notify to resolve vim.notify overwrite

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -3,9 +3,12 @@ return {
   event = "VeryLazy",
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    -- "rcarriga/nvim-notify",
   },
   opts = {
+    notify = {
+      enabled = false, -- This disables Noice's vim.notify override
+    },
     presets = {
       lsp_doc_border = true,
       bottom_search = true,
